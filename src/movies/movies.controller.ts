b48928d7aeef6a5ea208798003c7b0f187b74bff
High-East <rhehd127@gmail.com>
2022-04-14 23:51:29 +0900

    Initial commit

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Delete,
+  Patch,
+  Body,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+  // Service 모듈 사용
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
